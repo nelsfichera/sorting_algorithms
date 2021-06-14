ac7e2e43e04ea4f7757412079afc3e86ca2b7e32
Nels <nelsfichera@gmail.com>
2021-06-14 09:02:05 -0400

    adv setup

--- sort.h
+++ sort.h
@@ -25,4 +25,11 @@ void quick_sort(int *array, size_t size);
 void execute(int *array, size_t size, size_t total_size, int *aux);
 size_t partition(int *array, size_t low, size_t high, int *aux, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 #endif
